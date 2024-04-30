0f3ec0766ace01ab5717d0b8def9752f760f4342
nagaveda <nagaveda@nagavedasLaptop.attlocal.net>
2024-04-30 02:15:10 -0500

    Blinds fixes

--- Assets/Scripts/blinds.cs
+++ Assets/Scripts/blinds.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class blinds : MonoBehaviour
 {
-    public static float X;
-    public static float target_X = 0f;
-    public const float rot_speed = 1f;
+    private float rotationX = 85f;
+    private float targetRotationX = 0f;
+    private const float rotationSpeed = 100f;
     public static GameObject selectedBlind;
     public static GameObject selectedBlind_copy;
-    //public bool is_blinds_open =false;
+    public static bool is_blinds_open =false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,97 +20,43 @@ public class blinds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (is_blinds_open)
+        {
+            if (Mathf.Approximately(rotationX, 0f))
+            {
+                targetRotationX = 85f;
+            }
+        }
+        else 
+        {
+            if (Mathf.Approximately(rotationX, 85f))
+            {
+                targetRotationX = 0f;
+            }
+        }
+
+        rotationX = Mathf.MoveTowards(rotationX, targetRotationX, rotationSpeed * Time.deltaTime);
+        foreach (Transform child in transform)
+        {
+            // Operate on the local rotation of each child object
+            child.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        }
     }
 
     public static void BlindsOpen()
     {
-        X = 85f;
-
-        Debug.Log("open clicked ");
-
-        if (selectedBlind != null)
-        {
-            selectedBlind_copy = selectedBlind;
-            blinds blinds_script = selectedBlind_copy.GetComponent<blinds>();
-
-            if (blinds_script != null)
-            {
-                Debug.Log("enabled blind script for " + selectedBlind_copy.name);
-                blinds_script.enabled = true;
-            }
-
-            if (blinds_script.enabled)
-            {
-                Debug.Log("Entered math open function and x  value is"+X);
-
-                target_X = 0f;
-                X = Mathf.MoveTowards(X, target_X, rot_speed * Time.deltaTime);
-                foreach (Transform child in selectedBlind.transform)
-                {
-                    if (child.name.Contains("Cube"))
-                    {
-                        child.localRotation = Quaternion.Euler(X, 0f, 0f);
-                    }
-                }
-            }
-
-            // DashboardController.isblindon = true;
-        }
-
+        is_blinds_open = true;
         CharacterHandler.exitBlindMenu();
 
     }
 
     public static void BlindsClose()
     {
-        X = 0f;
-
-        Debug.Log("close clicked ");
-
-        if (selectedBlind != null)
-        {
-            selectedBlind_copy = selectedBlind;
-            blinds blinds_script = selectedBlind_copy.GetComponent<blinds>();
-
-            if (blinds_script != null)
-            {
-                Debug.Log("enabled blind script for " + selectedBlind_copy.name);
-                blinds_script.enabled = true;
-            }
-
-            if (blinds_script.enabled)
-            {
-                Debug.Log("Entered math open function and x  value is" + X);
-
-                target_X = 85f;
-                X = Mathf.MoveTowards(X, target_X, rot_speed * Time.deltaTime);
-                foreach (Transform child in selectedBlind.transform)
-                {
-                    if (child.name.Contains("Cube"))
-                    {
-                        child.localRotation = Quaternion.Euler(X, 0f, 0f);
-                    }
-                }
-            }
-
-            // DashboardController.isblindon = true;
-        }
-
+        is_blinds_open = false;
         CharacterHandler.exitBlindMenu();
 
         // DashboardController.isblindon = false;
     }
 
-    //public static void BlindRotate()
-    //{
-      //  X = Mathf.MoveTowards(X, target_X, rot_speed * Time.deltaTime);
-        //foreach (Transform child in selectedBlind.transform)
-        //{
-          //  if (child.name.Contains("Cube"))
-            //{
-              //  child.localRotation = Quaternion.Euler(X, 0f, 0f);
-            //}
-        //}
-
-    //}
+    
 }
